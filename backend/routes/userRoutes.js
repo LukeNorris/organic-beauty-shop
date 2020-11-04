1d02edfeff17570627ff4cfc66363bce3fe70d22
Luke Norris <luke.mjn@gmail.com>
2020-11-04 10:42:27 +0100

    added checkout, shipping, and placeorder screens

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,42 @@
+// import express from 'express'
+// import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
+// import { protect } from '../middleware/authMiddleware.js'
+
+// const router = express.Router()
+
+
+// router.post('/login', authUser)
+
+// router.route('/').post(registerUser)
+
+// router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+
+
+// export default router
+
+
 import express from 'express'
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
-
 const router = express.Router()
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers
 
 
-router.post('/login', authUser)
+} from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').post(registerUser)
-
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-
+router
+    .route('/')
+    .post(registerUser)
+    .get(protect, admin, getUsers)
+router
+    .post('/login', authUser)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 export default router
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productDetailsReducer, } from './reducers/productReducers.js'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers.js'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers.js'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers.js'
 import { cartReducer } from './reducers/cartReducers'
 
 
@@ -11,16 +11,30 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
-    userRegister: userRegisterReducer
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer
+
 })
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
 
-const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {}
 
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage },
+    cart: { 
+        cartItems: cartItemsFromStorage,
+        shippingAddress: shippingAddressFromStorage
+        
+    },
     userLogin: {userInfo: userInfoFromStorage}
 }
 
